80fd95899d31f269215b027a37b5f6d55e31a0ac
Steven Andrade <40500201+stev1494@users.noreply.github.com>
2020-04-02 20:17:00 -0500

    Configurando Socket.io

--- clasess/server.ts
+++ clasess/server.ts
@@ -1,18 +1,46 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 
 export default class Server {
+
+    //DPara definir el singleton
+    private static _instance: Server;
+
+
     public app: express.Application;
     public port:number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
-    constructor(){
+
+    private constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app);
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
+    }
+
+    public static get instance(){
+        return this._instance || ( this._instance = new this() );
+    }
+
+
+    private escucharSockets(){
+        console.log('Escuchando conexiones');
+
+        this.io.on('connection', cliente =>{
+            console.log('Cliente conectado');
+        })
     }
 
     start ( callback: any ){
-        this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
     }
 }
